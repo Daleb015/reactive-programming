7acabdfbe12021230fd8d79de9579356e6cbb40f
daleb <daleb015@gmail.com>
2021-10-14 14:36:43 -0500

    flatMap and concatMap

---
+++ combining-observables/src/main/java/org/example/combining_observables/FlatMapConcatMap.java
@@ -0,0 +1,22 @@
+package org.example.combining_observables;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.List;
+
+public class FlatMapConcatMap {
+    public static void main(String[] args) {
+
+        List<String> list = List.of("Hello","Reactive","Programming");
+
+        Observable.fromIterable(list)
+                .flatMap( e -> Observable.fromArray(e.split("")))
+                .subscribe(System.out::println);
+
+
+        Observable.fromIterable(list)
+                .concatMap( e -> Observable.fromArray(e.split("")))
+                .subscribe(System.out::println);
+
+    }
+}
