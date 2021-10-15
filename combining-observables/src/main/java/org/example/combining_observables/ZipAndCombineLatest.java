3e029e2630d6bbbfa254b3028fea60ae3300f8f4
daleb <daleb015@gmail.com>
2021-10-15 07:53:47 -0500

    Zip and combineLatest

---
+++ combining-observables/src/main/java/org/example/combining_observables/ZipAndCombineLatest.java
@@ -0,0 +1,27 @@
+package org.example.combining_observables;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class ZipAndCombineLatest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Observable<Long> src1 = Observable.interval(200, TimeUnit.MILLISECONDS).take(10);
+
+        Observable<Long> src2 = Observable.interval(1,TimeUnit.SECONDS).take(10);
+
+        Observable.zip(src1,src2,(e1,e2)->"Source 1 :"+e1+" Source 2 : "+e2)
+                .subscribe(System.out::println);
+
+        Thread.sleep(20000);
+
+        Observable.combineLatest(src1,src2,(e1,e2)->"Source 1 :"+e1+" Source 2 : "+e2)
+                .subscribe(System.out::println);
+
+        Thread.sleep(20000);
+
+    }
+
+}
