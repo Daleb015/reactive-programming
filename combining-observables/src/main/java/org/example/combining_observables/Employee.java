f20968ac16c3901416bf4784b106c77a5c0becc5
daleb <daleb015@gmail.com>
2021-10-15 12:54:07 -0500

    Grouping observables

---
+++ combining-observables/src/main/java/org/example/combining_observables/Employee.java
@@ -0,0 +1,58 @@
+package org.example.combining_observables;
+
+public class Employee {
+
+    private Integer code;
+    private String name;
+    private Integer salary;
+    private Double qualification;
+
+    public Employee(Integer code, String name, Integer salary, Double qualification) {
+        this.code = code;
+        this.name = name;
+        this.salary = salary;
+        this.qualification = qualification;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Double getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(Double qualification) {
+        this.qualification = qualification;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", qualification=" + qualification +
+                '}';
+    }
+}
