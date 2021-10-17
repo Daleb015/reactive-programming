8243d6c6fb2de05387bf38de6b809ebdd544a4b4
daleb <daleb015@gmail.com>
2021-10-16 22:20:10 -0500

    buffering - throttling switching

---
+++ buffering-throttling-switching/src/main/java/org/example/buffering_throttling_switching/Switching.java
@@ -0,0 +1,25 @@
+package org.example.buffering_throttling_switching;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableError;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class Switching {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<String> source = Observable.just("John","Lily","Emma","Reyan","Darshin")
+                .concatMap(
+                        s -> Observable.just(s)
+                                .delay(
+                                        ThreadLocalRandom.current().nextInt(1000), TimeUnit.MILLISECONDS
+                                )
+                );
+
+        Observable.interval(2,TimeUnit.SECONDS)
+                .switchMap(s -> source)
+                .subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+}
